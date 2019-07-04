4e839b4091fba6c4811974c5cab7ab2c4f9a74d3
Antonio Araujo <araujob.antonio@gmail.com>
2019-07-04 11:43:24 +0200

    added test images and modified the source code to include copy and clone

--- source/testing_mat.cpp
+++ source/testing_mat.cpp
@@ -10,8 +10,33 @@ int main(int argc, char** argv)
 
 	cv::Mat A,C;	// creates just the header parts
 	A = cv::imread(argv[1], cv::IMREAD_COLOR);	// here we'll know the method used (allocate matrix)
-	printf("changes were actually applied!\n");	
-	
+	//printf("changes were actually applied!\n");	
+
+	cv::Mat B(A);	// Use the copy constructor
+
+	C = A;	// Assignment operator
+
+	// Creating Mat objects with header refering to a subsection of the full data.
+	cv::Mat D (A, cv::Rect(100, 100, 200, 200) );	// using a rectangle
+	cv::Mat E = A(cv::Range::all(), cv::Range(100,200));	// using row and column boundaries	
+
+	// To make an actual copy of an image, so that we can modify it  without affecting the original Mat object:
+	cv::Mat F = A.clone();
+	cv::Mat G;
+	A.copyTo(G);	
+
+	// Show the images
+	cv::namedWindow("Image A", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Image A", A);
+	cv::namedWindow("Image B", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Image B", B);
+	cv::namedWindow("Image C", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Image C", C);	
+	cv::namedWindow("Image D", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Image D", D);	
+	cv::namedWindow("Image E", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Image E", E);	
+
 	cv::waitKey(0);
 
 	return 0;
