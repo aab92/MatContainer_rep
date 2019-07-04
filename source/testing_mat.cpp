4734c7adb027a9e1502dc90c4efaa2ed3127938f
Antonio Araujo <araujob.antonio@gmail.com>
2019-07-04 14:23:40 +0200

    Finished tutorial Mat - The Basic Image Container

--- source/testing_mat.cpp
+++ source/testing_mat.cpp
@@ -28,14 +28,42 @@ int main(int argc, char** argv)
 	// Mat constructor
 	cv::Mat M(2,2, CV_8UC3, cv::Scalar(0,0,255) );
 	std::cout << "M = " << std::endl << " " << M << std::endl << std::endl;
+	
+	// Mat create function. Does no allow initialization
+	cv::Mat L;
+	L.create(4,4, CV_8UC(2));
+	std::cout << "L = " << std::endl << " " << L << std::endl << std::endl;
+	
+	// MATLAB style initializer
+	E = cv::Mat::eye(4,4, CV_64F);
+	std::cout << "E = " << std::endl << " " << E << std::endl << std::endl;	// notice E is overwritten
+    cv::Mat O = cv::Mat::ones(2, 2, CV_32F);
+    std::cout << "O = " << std::endl << " " << O << std::endl << std::endl;
+    cv::Mat Z = cv::Mat::zeros(3,3, CV_8UC1);
+    std::cout << "Z = " << std::endl << " " << Z << std::endl << std::endl;	
+	
+	// Folllowing is possible for small matrices
+	C = (cv::Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+    //std::cout << "C = " <<  std::endl << " " << C <<  std::endl <<  std::endl;
+	C = (cv::Mat_<double>({ 0, -1, 0, -1, 5, -1, 0, -1, 0})).reshape(3);
+    std::cout << "C = " <<  std::endl << " " << C <<  std::endl <<  std::endl;
+ 	
+	//Create new object from the row of an existing ones
+	cv::Mat RowClone = C.row(1).clone();	// zero-based
+	std::cout << "RowClone = " << std::endl << " " << RowClone << std::endl << std::endl;
+	
+	// To fill a matrix with radom values, specifying the limit values
+	cv::Mat R = cv::Mat(3, 2, CV_8UC3 );
+	cv::randu(R, cv::Scalar::all(0), cv::Scalar::all(255));
+	std::cout << "R = " << std::endl << " " << R << std::endl << std::endl;
 
 	// Show the images
 	cv::namedWindow("Image A", cv::WINDOW_AUTOSIZE);
 	cv::imshow("Image A", A);
-	cv::namedWindow("Image B", cv::WINDOW_AUTOSIZE);
-	cv::imshow("Image B", B);
-	cv::namedWindow("Image C", cv::WINDOW_AUTOSIZE);
-	cv::imshow("Image C", C);	
+	//cv::namedWindow("Image B", cv::WINDOW_AUTOSIZE);
+	//cv::imshow("Image B", B);
+	//cv::namedWindow("Image C", cv::WINDOW_AUTOSIZE);
+	//cv::imshow("Image C", C);	
 	cv::namedWindow("Image D", cv::WINDOW_AUTOSIZE);
 	cv::imshow("Image D", D);	
 	cv::namedWindow("Image E", cv::WINDOW_AUTOSIZE);
